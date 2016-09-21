dabfa33ee980ccc3f392cf1d371690c6ed65591e
Adrian Hintermaier <adrian.hintermaier@gmail.com>
2016-09-21 14:53:01 +0200

    versioning

--- blog_api_media/blog/views.py
+++ blog_api_media/blog/views.py
@@ -79,4 +79,4 @@ class StatusView(APIView):
     def get(self, request, *args, **kwargs):
         # just return a JSON like this: {"version": "v1"}
         # getting the API version from the request
-        pass
+        return Response({"version": "v1"})
